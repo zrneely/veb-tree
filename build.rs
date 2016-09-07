788f041415fec378a3579e1b8d0bddd09d8f6f0e
Zachary Neely <zrussellneely@gmail.com>
2016-09-07 18:13:23 -0400

    Mark the old version name as deprecated

---
+++ build.rs
@@ -0,0 +1,5 @@
+
+fn main() {
+    panic!("This package has been renamed to \"veb\". Please use that package instead. It is also \
+           at version 1.0.0, providing additional stability and features.");
+}
